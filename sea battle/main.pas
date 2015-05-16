479128d1cbe5d81358fc9a9af8e0c3ffb5197088
Rermanessa <wishnewski@mail.ru>
2015-05-17 00:34:24 +1100

    123

--- sea battle/main.pas
+++ sea battle/main.pas
@@ -6,16 +6,59 @@ interface
 
 uses
   Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, ExtCtrls,
-  Grids;
+  Grids, StdCtrls;
 
 type
 
   { TMainForm }
 
   TMainForm = class(TForm)
+    Label1: TLabel;
+    Label10: TLabel;
+    Label11: TLabel;
+    Label12: TLabel;
+    Label13: TLabel;
+    Label14: TLabel;
+    Label15: TLabel;
+    Label16: TLabel;
+    Label17: TLabel;
+    Label18: TLabel;
+    Label19: TLabel;
+    Label2: TLabel;
+    Label20: TLabel;
+    Label21: TLabel;
+    Label22: TLabel;
+    Label23: TLabel;
+    Label24: TLabel;
+    Label25: TLabel;
+    Label26: TLabel;
+    Label27: TLabel;
+    Label28: TLabel;
+    Label29: TLabel;
+    Label3: TLabel;
+    Label30: TLabel;
+    Label31: TLabel;
+    Label32: TLabel;
+    Label33: TLabel;
+    Label34: TLabel;
+    Label35: TLabel;
+    Label36: TLabel;
+    Label37: TLabel;
+    Label38: TLabel;
+    Label39: TLabel;
+    Label4: TLabel;
+    Label40: TLabel;
+    Label5: TLabel;
+    Label6: TLabel;
+    Label7: TLabel;
+    Label8: TLabel;
+    Label9: TLabel;
     UserGrid: TDrawGrid;
     CompGrid: TDrawGrid;
     ResultPanel: TPanel;
+    procedure FormCreate(Sender: TObject);
+    procedure Label18Click(Sender: TObject);
+    procedure Label1Click(Sender: TObject);
   private
     { private declarations }
   public
@@ -28,6 +71,23 @@ var
 implementation
 
 {$R *.lfm}
+
+{ TMainForm }
+
+procedure TMainForm.FormCreate(Sender: TObject);
+begin
+
+end;
+
+procedure TMainForm.Label18Click(Sender: TObject);
+begin
+
+end;
+
+procedure TMainForm.Label1Click(Sender: TObject);
+begin
+
+end;
 
 end.
 
